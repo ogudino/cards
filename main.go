cefc320d25bb67e72857ba9afb39040152d6b282
ogudino <76851139+ogudino@users.noreply.github.com>
2022-08-04 16:53:13 -0400

    created func shuffle()

--- main.go
+++ main.go
@@ -1,12 +1,8 @@
 package main
 
 func main() {
-	// cards := newDeck()
-
-	// hand, remainingCards := deal(cards, 5) //return two values
-
-	// hand.print()
-	// remainingCards.print()
-	cards := newDeckFromFile("my_cards")
+	cards := newDeck()
+	cards.shuffle()
 	cards.print()
+
 }
