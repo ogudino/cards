8e05cca9a1830f9525559ed563a2202d79c22fdd
ogudino <76851139+ogudino@users.noreply.github.com>
2022-08-04 17:03:50 -0400

    fixed func shuffle()

--- deck.go
+++ deck.go
@@ -6,6 +6,7 @@ import (
 	"math/rand"
 	"os"
 	"strings"
+	"time"
 )
 
 // create a new type of 'deck'
@@ -61,8 +62,11 @@ func newDeckFromFile(filename string) deck {
 }
 
 func (d deck) shuffle() {
+	source := rand.NewSource(time.Now().UnixNano()) //use UnixNano to always get int64 number
+	r := rand.New(source)
+
 	for i := range d {
-		newPosition := rand.Intn(len(d) - 1)
+		newPosition := r.Intn(len(d) - 1)
 
 		d[i], d[newPosition] = d[newPosition], d[i] //swap elements
 	}
